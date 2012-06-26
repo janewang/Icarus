438fbae2a9b16d27aefd3e8bb79b3a8f50021864
Jane Wang <janelwang2008@gmail.com>
2012-06-26 02:01:29 -0400

    Better Icarus refresh

--- assets/js/icarus.js
+++ assets/js/icarus.js
@@ -1,38 +1,41 @@
 var Icarus = function(canvas) {
-  var context = canvas.getContext('2d'); 
   var img = new Image();
   img.src = '/images/kid_icarus.png';
+  this.context = canvas.getContext('2d'); 
 
   this.draw = function(x, y) {
-    img.onload = function(){
-      context.drawImage(img,x,y);
-    }
-  } 
+    this.context.drawImage(img, x, y);
+  }
+  
+  this.end = function() {
+    this.context.fillStyle = 'rgba(255,0,0,0.85)';
+    this.context.font = '40pt Helvetica';
+    this.context.textAlign = 'center';
+    this.context.textBaseline = 'center';
+    this.context.fillText('GAME OVER', canvas.width/2, canvas.height/2);
+  }
 }
 
 function fly() {
   var canvas = $('#particles')[0];
+  var a = new Icarus(canvas);
 
   // border limit
   $(window).bind('mousemove', function(e){
     var x, y;
-    if(e.pageX < canvas.offsetLeft + 25) {
-      x = 0;
-    } else if(e.pageX - canvas.offsetLeft > canvas.width - 25) {
-      x = canvas.width - 25;
-    } else{
-      x = e.pageX - canvas.offsetLeft;
+    switch (true) {
+      case (e.pageX < canvas.offsetLeft + 25): x = 0; break;
+      case (e.pageX - canvas.offsetLeft > canvas.width - 25): x = canvas.width - 25; break;
+      default: x = e.pageX - canvas.offsetLeft;
     }
 
-    if(e.pageY < canvas.offsetTop + 33) {
-      y = 0;
-    } else if(e.pageY - canvas.offsetTop > canvas.height - 33) {
-      y = canvas.height - 33;
-    } else{
-      y = e.pageY - canvas.offsetTop;
+    switch (true) {
+      case (e.pageY < canvas.offsetTop + 33): y = 0; break;
+      case (e.pageY - canvas.offsetTop > canvas.height - 33): y = canvas.height - 33; break;
+      default: y = e.pageY - canvas.offsetTop;
     }
 
-    var a = new Icarus(canvas);
     a.draw(x,y);
+    // a.end();
   });
 }
