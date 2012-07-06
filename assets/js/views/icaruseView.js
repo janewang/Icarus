f4d05a432f29c981456808c1d63b6cf082e35d15
Jane Wang <janelwang2008@gmail.com>
2012-07-06 19:12:07 -0400

    deplay Icarus canvas rendering until game starts

--- assets/js/views/icaruseView.js
+++ assets/js/views/icaruseView.js
@@ -2,11 +2,12 @@ var PlayerListView = Backbone.View.extend({
   el: $('body'),
   
   events: {
-    'mousemove': 'fly'
+    'click a#start'     : 'start', 
+    'keypress'          : 'start',
   },
   
   initialize: function(){
-    _.bindAll(this, 'fly', 'draw');
+    _.bindAll(this, 'start', 'draw', 'fly');
     var _this = this;
     this.icarusCollection = new IcarusCollection(1);
 
@@ -21,30 +22,37 @@ var PlayerListView = Backbone.View.extend({
     this.img.src = '/images/kid_icarus.png';
   },
   
+  start: function(x, y) {
+    this.fly();
+  },
+  
   draw: function(x, y) {
     this.context.drawImage(this.img, x, y);
   },
   
-  fly: function(e){
-    var x, y;
-    
-    switch (true) {
-      case (e.pageX < this.canvas.offsetLeft + 25): x = 0; break;
-      case (e.pageX - this.canvas.offsetLeft > this.canvas.width - 25): x = this.canvas.width - 25; break;
-      default: x = e.pageX - this.canvas.offsetLeft;
-    }
+  fly: function(){
+    var _this = this;
+    $(window).bind('mousemove', function(e) {
+      var x, y;
 
-    switch (true) {
-      case (e.pageY < this.canvas.offsetTop + 33): y = 0; break;
-      case (e.pageY - this.canvas.offsetTop > this.canvas.height - 33): y = this.canvas.height - 33; break;
-      default: y = e.pageY - this.canvas.offsetTop;
-    }
-    
-    // save mouse last move
-    this.position = {x: x, y: y};
-    this.socket.emit('icarus position', this.position);
-    this.draw(x, y);
-  }
+      switch (true) {
+        case (e.pageX < _this.canvas.offsetLeft + 25): x = 0; break;
+        case (e.pageX - _this.canvas.offsetLeft > _this.canvas.width - 25): x = _this.canvas.width - 25; break;
+        default: x = e.pageX - _this.canvas.offsetLeft;
+      }
+
+      switch (true) {
+        case (e.pageY < _this.canvas.offsetTop + 33): y = 0; break;
+        case (e.pageY - _this.canvas.offsetTop > _this.canvas.height - 33): y = _this.canvas.height - 33; break;
+        default: y = e.pageY - _this.canvas.offsetTop;
+      }
+      
+      // save mouse last move
+      _this.position = {x: x, y: y};
+      _this.socket.emit('icarus position', this.position);
+      _this.draw(x, y);   
+    });
+  } 
 });
 
 var playerListView = new PlayerListView();
