a121700cc5026fdc2c001e9c150557df3cc2cfec
Jane Wang <janelwang2008@gmail.com>
2012-07-06 17:07:15 -0400

    Updated universe model, collection and view

--- assets/js/models/particle.js
+++ assets/js/models/particle.js
@@ -5,9 +5,4 @@ window.ParticleCollection = Backbone.Collection.extend({
     model: Particle
 });
 
-var particleCollection = new ParticleCollection();
 
-var socket = io.connect('http://localhost');
-socket.on('particle position', function (data) {
-  particleCollection.reset(data);
-});
--- assets/js/views/particleView.js
+++ assets/js/views/particleView.js
@@ -1,38 +1,49 @@
 var UniverseView = Backbone.View.extend({
     el: $('body'),
     
-    // events: {
-    //     'click button#start': 'draw', 
-    //     'keypress'          : 'draw'  
-    // },
+    events: {
+        'click a#start'     : 'start', 
+        'keypress'          : 'start'  
+    },
     
-    initialize: function(){
+    initialize: function() {
+      _.bindAll(this, 'start');
+      this.canvas = $('#particles')[0]; 
+      this.context = this.canvas.getContext('2d');
       
-      // _.bindAll(this, 'draw');
-        this.collection = particleCollection;
-        this.collection.on('reset', function(){
-        var canvas = $('#particles')[0]; 
-        var context = canvas.getContext('2d');
+      this.particleCollection = new ParticleCollection();
+      var _this = this;
+
+      var socket = io.connect('http://' +   window.location.hostname);
+      socket.on('particle position', function (data) {
+        _this.particleCollection.reset(data);
+      });
+    },
+
+    start: function() {
+      var _this = this;
+      this.particleCollection.on('reset', function() {
 
         // fading
-        context.globalCompositeOperation = 'source-in';
-        context.fillStyle = "rgba("+
+        _this.context.globalCompositeOperation = 'source-in';
+        _this.context.fillStyle = "rgba("+
           Math.floor(Math.random()*256)+","+
           Math.floor(Math.random()*256)+","+
           Math.floor(Math.random()*256)+",0.85)";
-        context.fillRect(0, 0, canvas.width, canvas.height);  
+        _this.context.fillRect(0, 0, _this.canvas.width, _this.canvas.height);
 
         // dot drawing style
-        context.globalCompositeOperation = 'lighter';
-        context.fillStyle = 'rgba(255,255,255,0.85)';
+        _this.context.globalCompositeOperation = 'lighter';
+        _this.context.fillStyle = 'rgba(255,255,255,0.85)';
 
         // update this universe based on collection
-         _.each(particleCollection.models, function(particle) {
-           context.beginPath();
-           context.arc(particle.attributes.x, particle.attributes.y, 2.5, 0, Math.PI*2, false);
-           context.fill();  
+         _.each(_this.particleCollection.models, function(particle) {
+           _this.context.beginPath();
+           _this.context.arc(particle.get('x'), particle.get('y'), 2.5, 0, Math.PI*2, false);
+           _this.context.fill();  
          });
       });
+      console.log('started');
     }
 });
 
